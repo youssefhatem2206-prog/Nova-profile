c98fc326bbe5e42cb48d898dcfea94f0f8b5aac1
Youssef Hatem <90394292+youssef13494@users.noreply.github.com>
2024-10-26 17:55:16 +0300

    Add New projects

--- script/script.js
+++ script/script.js
@@ -17,8 +17,30 @@ window.addEventListener('scroll', function() {
     const rect = contactSection.getBoundingClientRect();
     
     if (rect.top <= window.innerHeight && rect.bottom >= 0) {
-        footer.style.display = 'block'; // إظهار الفوتر
+        footer.style.display = 'block'; 
     } else {
-        footer.style.display = 'none'; // إخفاء الفوتر
+        footer.style.display = 'none'; 
     }
 });
+
+function sendEmail() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const subject = document.getElementById('subject').value;
+    const comment = document.getElementById('comment').value;
+    
+    emailjs.send("service_g4pd0uf","template_obwymbh",{
+        from_name: "Youssef",
+        to_name: name,
+        message: subject,
+        email: email,
+        reply_to:comment,
+        })
+        .then((response) => {
+            console.log('SUCCESS!', response.status, response.text);
+            alert('Your message has been sent!'); // Confirmation message
+        }, (error) => {
+            console.log('FAILED...', error);
+            alert('Failed to send your message. Please try again later.');
+        });
+}
